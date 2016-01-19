1a8573865bad1caffa02fbe785d6343b04dee2d5
Levi Rodriguez <g.levi.rodriguez@gmail.com>
2016-01-19 15:13:00 -0700

    Added general packages

---
+++ src/model/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * General package for model.
+ *
+ * Created by gLeviRodriguez on 1/19/2016.
+ */
+package model;
---
+++ src/server/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * General package for server.
+ *
+ * Created by gLeviRodriguez on 1/19/2016.
+ */
+package server;
