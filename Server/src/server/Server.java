b6bb9e7bc791f11be417847288ba9abe520ad7e9
rayn33 <ray.nelson@att.net>
2016-02-02 13:56:37 -0700

    additions to server
    -put what client gives server into a json object
    -Finished the skeleton of the server class.

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -6,15 +6,22 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Server {
 	
 //------CLASS VARIABLES BELOW--------------------------------------
 //-----------------------------------------------------------------
 	
+	//SET OF USERS BELOW NOT TO BE USED RIGHT NOW
     private static HashSet<User> users=new HashSet<User>(); //populate this guy from the run method of each thread.
     private static int ID_to_give_out=0;
+    private static Map<String, PrintWriter> user_to_printwriter=new HashMap<String, PrintWriter>();
     
 //-----------------------------------------------------------------
 //------END OF CLASS VARIABLES-------------------------------------
@@ -57,7 +64,7 @@ public class Server {
 //------------------------------------------------------------------------------
     
     private static class Connection_Manager extends Thread {
-        private String name;
+        private String login_credentials;
         private Socket socket;
         private BufferedReader in;
         private PrintWriter out;
@@ -82,6 +89,44 @@ public class Server {
 	            out = new PrintWriter(socket.getOutputStream(), true);
 	            
 	            User user;
+	            
+	            while(true)  //this is where we wait till a user is successfully logged in.
+	            {
+	            	login_credentials = in.readLine();
+	                if (login_credentials == null) {
+	                   return;
+	                }
+	                synchronized (user_to_printwriter)
+	                {
+	                	try {
+	                		
+							JSONObject login=new JSONObject(login_credentials);  //make a json out of what client gave server
+							String username=login.getString("username");
+							
+							if(!user_to_printwriter.containsKey(username));		//make sure that user isn't already logged in
+							{
+								user_to_printwriter.put(username, out);
+								
+								break;
+							}
+							
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+	                }
+	            }
+	            
+	            while(true)		//this is where we listen after a user is logged in.
+	            {
+	            	String input = in.readLine();
+                    if (input == null) {
+                        return;
+                    }
+                    
+                    //here, figure out what the client just said to you and hand it off to 
+                    //command factory or facade or whoever is supposed to get it.
+	            }
         	} 
         	catch(IOException e)
         	{
