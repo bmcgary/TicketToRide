c53fceced7be68ffdc7bc7f8046fbef916ab3535
Levi Rodriguez <g.levi.rodriguez@gmail.com>
2016-02-11 15:22:29 -0700

    implemented responsewrapper and basic response

--- Server/src/server/responses/ResponseWrapper.java
+++ Server/src/server/responses/ResponseWrapper.java
@@ -2,6 +2,7 @@ package server.responses;
 
 import server.JsonTranslator;
 
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -10,8 +11,8 @@ import java.util.List;
  * Created by rodriggl on 2/4/2016.
  */
 public class ResponseWrapper {
-    List<Integer> targetIDs;
-    String response;
+    private List<Integer> targetIDs;
+    private String response;
 
     public ResponseWrapper(List<Integer> targetIDs, Response response) {
         this.targetIDs = targetIDs;
@@ -21,5 +22,13 @@ public class ResponseWrapper {
     public ResponseWrapper(List<Integer> targetIDs, String response) {
         this.targetIDs = targetIDs;
         this.response = response;
+    }
+
+    public Iterator<Integer> getTargetIds() {
+        return targetIDs.iterator();
+    }
+
+    public String getResponse() {
+        return response;
     }
 }
