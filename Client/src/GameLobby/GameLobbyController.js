520da0636ac5474a806da715492d5ed03e654ca0
Dantley Pearce Rogers <dantleypearcerogers@Dantleys-MacBook-Pro.local>
2016-02-24 13:57:43 -0700

    here is the simple lay out of the game lobby, i still dont understand how to add this to the UI router stuff. but this is is the simple layour lacking css

---
+++ Client/src/GameLobby/GameLobbyController.js
@@ -0,0 +1,83 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    var myGames = [
+      {
+        id:"10",
+        players:[
+          "John",
+          "Ben",
+          "Kyle"
+        ],
+        yourColor:"red"
+      },
+      {
+        id:"11",
+        players:[
+          "Josh",
+          "Tyler",
+          "Jared"
+        ],
+        yourColor:"pink"
+      },
+      {
+        id:"12",
+        players:
+        [
+          "Fred",
+          "Ron",
+          "Ben"
+        ],
+        yourColor:"Blue"
+      }
+  ];
+  var availableGames = [
+    {
+      id:"10",
+      players:[
+        "John",
+        "Ben",
+        "Kyle"
+      ],
+      colorAvailable:[
+        "blue",
+        "red",
+        "Orange"
+      ]
+    },
+    {
+      id:"11",
+      players:[
+        "Josh",
+        "Tyler",
+        "Jared"
+      ],
+      colorAvailable:[
+        "blue",
+        "red",
+        "Orange"
+      ]
+    },
+    {
+      id:"12",
+      players:
+      [
+        "green",
+        "blue",
+        "red"
+      ],
+      colorAvailable:[
+        "yello",
+        "red",
+        "purple"
+      ]
+    }
+];
+    $scope.myGames = myGames;
+    $scope.availableGames = availableGames;
+
+
+  $scope.reJoinGame = function (id){
+    console.log("you are rejoing: "+id);
+  }
+
+});
