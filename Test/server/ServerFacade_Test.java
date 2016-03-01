636d0a56e659add8c4c74f3e061ef657df710ea2
chao wang <sathongkong@hotmail.com>
2016-03-01 09:48:25 +0800

    invert wrong push and it should be good now.

---
+++ Test/server/ServerFacade_Test.java
@@ -0,0 +1,159 @@
+package server;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ServerFacade_Test {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetServerFacade() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddNewUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanAddPlayerToGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPlayerToGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanStartGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanLeaveGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLeaveGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLogout() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanBuyRoute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuyRoute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanDrawTrainCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDrawTrainCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanGetDestinations() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDestinations() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCanSelectDestinations() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectDestinations() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadGameState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveGameState() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendClientModelInformation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCityMapping() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetJoinableGames() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserGames() {
+		fail("Not yet implemented");
+	}
+
+}
