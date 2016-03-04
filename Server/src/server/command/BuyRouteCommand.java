ab15af3c6c516dc4538f3f77c7c645a3d3bb00a1
rayn33 <ray.nelson@att.net>
2016-03-04 14:37:36 -0700

    Working on BuyRouteCommand

--- Server/src/server/command/BuyRouteCommand.java
+++ Server/src/server/command/BuyRouteCommand.java
@@ -1,5 +1,7 @@
 package server.command;
 
+import com.google.gson.annotations.SerializedName;
+
 import server.responses.Response;
 import server.responses.ResponseWrapper;
 
@@ -9,8 +11,14 @@ import server.responses.ResponseWrapper;
  * Created by rodriggl on 1/29/2016.
  */
 public class BuyRouteCommand extends Command {
+	@SerializedName("gameId")
+	private int gameID;
+	
     @Override
     public ResponseWrapper execute(int userID) {
+    	
+    	//serverFacade.buyRoute(userID, gameID, route);
+    	
         return new ResponseWrapper(userID, Response.newServerErrorResponse(), commandName);
     }
 }
