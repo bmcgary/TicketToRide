33f2d6ce8ba9b1d2b097d517eb6119d8acaf85be
rayn33 <ray.nelson@att.net>
2016-03-04 19:49:21 -0700

    adjustments to websockethandler and implemented buyroutecommand

--- Server/src/server/command/BuyRouteCommand.java
+++ Server/src/server/command/BuyRouteCommand.java
@@ -1,7 +1,16 @@
 package server.command;
 
+import java.util.List;
+
+import model.CityToCityRoute;
+import model.Game;
+import model.GameBoard;
+
 import com.google.gson.annotations.SerializedName;
 
+import server.exception.InternalServerException;
+import server.exception.OutOfBoundsException;
+import server.exception.PreConditionException;
 import server.responses.Response;
 import server.responses.ResponseWrapper;
 
@@ -13,12 +22,43 @@ import server.responses.ResponseWrapper;
 public class BuyRouteCommand extends Command {
 	@SerializedName("gameId")
 	private int gameID;
+	@SerializedName("routeIndex")
+	private int routeIndex;
 	
     @Override
     public ResponseWrapper execute(int userID) {
-    	
-    	//serverFacade.buyRoute(userID, gameID, route);
+
+    	CityToCityRoute route=getRoute();
+    	try {
+			serverFacade.buyRoute(userID, gameID, route);
+		} catch (PreConditionException | InternalServerException
+				| OutOfBoundsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	
         return new ResponseWrapper(userID, Response.newServerErrorResponse(), commandName);
+    }
+    
+    private CityToCityRoute getRoute()
+    {
+    	List<Game> games=serverFacade.getAllGames();
+    	Game game=null;
+    	
+    	for(int i=0; i<games.size(); i++)
+    	{
+    		if(games.get(i).getGameID()==gameID)
+    		{
+    			game=games.get(i);
+    			break;
+    		}
+    	}
+    	
+    	GameBoard board=game.getGameBoard();
+    	List<CityToCityRoute> routes=board.getRoutes();
+    	CityToCityRoute route=routes.get(routeIndex);
+    	
+    	return route;
+    		
     }
 }
