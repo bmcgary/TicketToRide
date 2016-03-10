7b4ba33d2e6d98d856d191456d9771ec4ccb9d36
Levi Rodriguez <g.levi.rodriguez@gmail.com>
2016-03-10 08:57:47 -0700

    Expanded game info; named track color

---
+++ Server/src/server/dto/CardInfo.java
@@ -0,0 +1,14 @@
+package server.dto;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Generalized Card Info class
+ * Created by rodriggl on 3/10/2016.
+ */
+public class CardInfo {
+    @SerializedName("color")
+    private String cardColor;
+    @SerializedName("amount")
+    private int amount;
+}
--- Server/src/server/dto/PlayerInfo.java
+++ Server/src/server/dto/PlayerInfo.java
@@ -17,11 +17,9 @@ public class PlayerInfo {
     @SerializedName("color")
     private String colorName;
     @SerializedName("playerOrder")
-    private int playerOrder;
-
-    public PlayerInfo(Player player, int playerOrder) {
-        this.username = serverFacade.getAllUsers().get(player.getPlayerID()).getUsername();
-        this.colorName = player.getPlayerColor().toString();
-        this.playerOrder = playerOrder;
-    }
+    private Integer playerOrder;
+    @SerializedName("trainsLeft")
+    private Integer trainsLeft;
+    @SerializedName("cards")
+    private CardInfo[] cardInfos;
 }
