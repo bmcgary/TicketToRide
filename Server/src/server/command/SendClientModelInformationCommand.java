da9b331417fdee7fc18e9b28bf8df3627dfc714d
Levi Rodriguez <g.levi.rodriguez@gmail.com>
2016-03-15 16:11:15 -0600

    Implemented

--- Server/src/server/command/SendClientModelInformationCommand.java
+++ Server/src/server/command/SendClientModelInformationCommand.java
@@ -1,8 +1,12 @@
 package server.command;
 
+import com.google.gson.annotations.SerializedName;
+import server.dto.gameplay.GamePlayInfo;
+import server.exception.GameNotFoundException;
 import server.responses.Response;
 import server.responses.ResponseWrapper;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,8 +16,43 @@ import java.util.List;
  * Created by rodriggl on 1/29/2016.
  */
 public class SendClientModelInformationCommand extends Command {
+    @SerializedName("gameId")
+    private int gameId;
+
+    private transient GamePlayInfo gamePlayInfo;
+
+    public SendClientModelInformationCommand(int gameId) {
+        this.gameId = gameId;
+    }
+
     @Override
     public List<ResponseWrapper> execute(int userID) {
-        return Collections.singletonList(new ResponseWrapper(userID, Response.newServerErrorResponse(), commandName));
+        return execute(Collections.singletonList(userID));
+    }
+
+    public List<ResponseWrapper> execute(List<Integer> playerIds) {
+        List<ResponseWrapper> responses = new ArrayList<>();
+        ResponseWrapper responseWrapper = new ResponseWrapper(commandName).setTargetIds(playerIds);
+        responses.add(responseWrapper);
+        if (gamePlayInfo == null) {
+            try {
+                gamePlayInfo = new GamePlayInfo(serverFacade.getGame(gameId));
+            } catch (GameNotFoundException e) {
+                responseWrapper.setResponse(Response.newServerErrorResponse());
+                return responses;
+            }
+        }
+        responseWrapper.setResponse(gamePlayInfo);
+
+        playerIds.parallelStream().forEach(playerId -> {
+            ResponseWrapper privateResponseWrapper = new ResponseWrapper(playerId, "PrivateClientModelInformation");
+            responses.add(privateResponseWrapper.setResponse(gamePlayInfo.getPrivateInfo(playerId)));
+        });
+
+        return responses;
+    }
+
+    public void setGamePlayInfo(GamePlayInfo gamePlayInfo) {
+        this.gamePlayInfo = gamePlayInfo;
     }
 }
--- Server/src/server/command/StartGameCommand.java
+++ Server/src/server/command/StartGameCommand.java
@@ -1,10 +1,15 @@
 package server.command;
 
 import com.google.gson.annotations.SerializedName;
+import model.Game;
+import server.dto.gameplay.GamePlayInfo;
+import server.exception.GameNotFoundException;
+import server.exception.InternalServerException;
+import server.exception.PreConditionException;
 import server.responses.Response;
 import server.responses.ResponseWrapper;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,7 +27,30 @@ public class StartGameCommand extends Command {
 
     @Override
     public List<ResponseWrapper> execute(int userID) {
+        List<ResponseWrapper> responses = new ArrayList<>();
+        ResponseWrapper responseWrapper = new ResponseWrapper(userID, commandName);
+        responses.add(responseWrapper);
 
-        return Collections.singletonList(new ResponseWrapper(userID, Response.newServerErrorResponse(), commandName));
+        Game game;
+        try {
+            serverFacade.startGame(userID, gameId);
+            game = serverFacade.getGame(gameId);
+        } catch (PreConditionException e) {
+            responseWrapper.setResponse(new Response("unable to start game"));
+            return responses;
+        } catch (InternalServerException | GameNotFoundException e) {
+            responseWrapper.setResponse(Response.newServerErrorResponse());
+            return responses;
+        }
+
+        GamePlayInfo gamePlayInfo = new GamePlayInfo(game);
+        List<Integer> playerIds = gamePlayInfo.getPlayerIds();
+        responseWrapper.setTargetIds(playerIds).setResponse(new Response("game has started"));
+
+        SendClientModelInformationCommand command = new SendClientModelInformationCommand(gameId);
+        command.setGamePlayInfo(gamePlayInfo);
+        responses.addAll(command.execute(playerIds));
+
+        return responses;
     }
 }
