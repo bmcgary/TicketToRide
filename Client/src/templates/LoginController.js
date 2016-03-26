ad9a419167edb4dd304773a036b72bc20c91277f
Drew Harker <harker.drew@gmail.com>
2016-03-26 12:41:22 -0600

    Fixed the login/register credential checks

--- Client/src/templates/LoginController.js
+++ Client/src/templates/LoginController.js
@@ -1,21 +1,44 @@
 var app = angular.module('ticketToRide');
 
 app.controller('loginController', function ($rootScope, $scope, $state, ClientAPI, ModelFacade) {
-    var username = "";
 
     $scope.submit = function () {
-        if($scope.username.length > 25 || $scope.username.length < 4 ||
-        $scope.password.length > 25 || $scope.password.length < 4) {
-            alert("Error: Invalid input constraints.");
-        } else {
-            this.username = $scope.username;
+        var badCredentials = false;
+
+        if($scope.username.length > 25)
+        {
+            alert("Error: Username exceeds the 25 characters");
+            badCredentials = true;
+        }
+        else if($scope.username.length < 4)
+        {
+            alert("Error: Username must be at least 4 characters");
+            badCredentials = true;
+        }
+
+        if($scope.password.length > 25)
+        {
+            alert("Error: Password exceeds the 25 characters");
+            badCredentials = true;
+        }
+        else if($scope.password.length < 4)
+        {
+            alert("Error: Password must be at least 4 characters");
+            badCredentials = true;
+        }
+
+        if(!badCredentials)
+        {
             ClientAPI.login($scope.username, $scope.password);
         }
     };
+
     $rootScope.$on('server:Login', function (event, parameters) {
-    	if(parameters.description == "success") {
+    	if(parameters.description == "success")
+    	{
     		$state.go('gameLobby');
-    	} else if (parameters.description == "alreadyloggedin") {
+    	}
+    	else if (parameters.description == "alreadyloggedin") {
     		alert("You are currently logged in already!");
             $state.go('gameLobby');
     	} else if(parameters.description == "invalid input") {
