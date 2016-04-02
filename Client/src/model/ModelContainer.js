fb49d1142f4705e125fc07180aa167ced1973495
bmgary <bmcgary@byu.edu>
2016-04-02 14:46:06 -0600

    the draw dest card and all wild now disabled on the second draw train card round

--- Client/src/model/ModelContainer.js
+++ Client/src/model/ModelContainer.js
@@ -143,7 +143,8 @@ app.factory('ModelContainer', function () {
     };
 
     ModelContainer.prototype.getTrainCardsByColor = function(trainCardColor) {
-		var convertedColor = (trainCardColor.charAt(0).toUpperCase() + trainCardColor.slice(1));
+		var convertedColor =  trainCardColor == 'wild' ? 'None' : trainCardColor;
+		convertedColor = (convertedColor.charAt(0).toUpperCase() + convertedColor.slice(1));
         var count = this.model.player.trainCards[convertedColor];
         if(typeof(count) === 'undefined' || typeof(count) === 'null') {
             count = 0;
