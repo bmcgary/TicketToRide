d737bef489cea003e2ffe31287a8cdafeb3f0d79
Drew Harker <harker.drew@gmail.com>
2016-04-05 18:52:00 -0600

    Refactored Client for syntax bug

--- Client/src/model/GameBoard.js
+++ Client/src/model/GameBoard.js
@@ -1,19 +1,16 @@
 var app = angular.module('ticketToRide');
 
 app.factory('GameBoard', function (TrainCardColor, StaticTrackList) {
-
-    GameBoard.prototype.deckHasDestinations = true;
-    GameBoard.prototype.deckHasTrains = true;
-    GameBoard.prototype.isFirstRound = true;
-    GameBoard.prototype.isLastRound = false;
-    GameBoard.prototype.routesPurchased = {}; //Map <routeIndex, playerId> //Maybe change playerId to Color
-    GameBoard.prototype.cardsVisible = []; //TrainCardColor[]
-    GameBoard.prototype.mustDrawAgain = false;
-    GameBoard.prototype.playerIdForTheLongestBonus = -1;
-
     //constructor 
     function GameBoard () {
-        
+        this.deckHasDestinations = true;
+        this.deckHasTrains = true;
+        this.isFirstRound = true;
+        this.isLastRound = false;
+        this.routesPurchased = {};
+        this.cardsVisible = [];
+        this.mustDrawAgain = false;
+        this.playerIdForTheLongestBonus = -1;
     }
 
     GameBoard.prototype.updateCardsVisible = function (availableTrainCards) {
@@ -34,6 +31,52 @@ app.factory('GameBoard', function (TrainCardColor, StaticTrackList) {
 
     GameBoard.prototype.addRoutePurchased = function (routeIndex, trainColor) {
         this.routesPurchased[routeIndex] = TrainCardColor.get(trainColor);
+    }
+
+    //Getters
+    GameBoard.prototype.getDeckHasDestinations = function () {
+        return this.deckHasDestinations;
+    }
+
+    GameBoard.prototype.getDeckHasTrains = function () {
+        return this.deckHasTrains;
+    }
+
+    GameBoard.prototype.getIsFirstRound = function () {
+        return this.isFirstRound;
+    }
+
+    GameBoard.prototype.getIsLastRound = function () {
+        return this.isLastRound;
+    }
+
+    GameBoard.prototype.getRoutesPurchased = function () {
+        return this.routesPurchased;
+    }
+
+    GameBoard.prototype.getCardsVisibleAt = function (index) {
+        return this.cardsVisible[index];
+    }
+
+    GameBoard.prototype.getMustDrawAgain = function () {
+        return this.mustDrawAgain;
+    }
+
+    GameBoard.prototype.getPlayerIdForTheLongestBonus = function () {
+        return this.playerIdForTheLongestBonus;
+    }
+
+    //Setters
+    GameBoard.prototype.setIsLastRound = function (last) {
+        this.isLastRound = last;
+    }
+
+    GameBoard.prototype.setMustDrawAgain = function (drawAgain) {
+        this.getMustDrawAgain = drawAgain;
+    }
+
+    GameBoard.prototype.setPlayerIdForTheLongestBonus = function (id) {
+        this.playerIdForTheLongestBonus = id;
     }
 
     return GameBoard;
